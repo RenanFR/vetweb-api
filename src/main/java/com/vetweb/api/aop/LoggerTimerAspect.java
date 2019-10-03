0a61dd6c45d2bf1ec5b09c08d616b3ff6c3b7769
Renan Rodrigues <renanfr1047@gmail.com>
2019-10-03 16:50:52 -0300

    Annotation aspect

---
+++ src/main/java/com/vetweb/api/aop/LoggerTimerAspect.java
@@ -0,0 +1,35 @@
+package com.vetweb.api.aop;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Aspect
+@Component
+@Slf4j
+public class LoggerTimerAspect {
+	
+	@Pointcut("execution(* com.vetweb.api.resources..*.*(..))")
+	public void everyMethod() {}
+	
+	@Around("everyMethod()")
+	public void logAndCount(ProceedingJoinPoint joinPoint) throws Throwable {
+		MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
+		log.info(String.format("Method %s called at %s, return type is %s", 
+				methodSignature.getName(), LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss")), methodSignature.getReturnType()));
+		long start = System.currentTimeMillis();
+		joinPoint.proceed();
+		long end = System.currentTimeMillis();
+		long timeToComplete = (start - end)/1000;
+		log.info(String.format("Method took %d to complete execution", timeToComplete));		
+	}
+	
+}
