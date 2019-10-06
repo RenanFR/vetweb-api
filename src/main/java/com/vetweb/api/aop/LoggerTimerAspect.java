622a05d057f146cf4ef7551bd06b9ecb503594b2
Renan Rodrigues <renanfr1047@gmail.com>
2019-10-05 23:06:20 -0300

    Fix on aspect

--- src/main/java/com/vetweb/api/aop/LoggerTimerAspect.java
+++ src/main/java/com/vetweb/api/aop/LoggerTimerAspect.java
@@ -19,17 +19,18 @@ public class LoggerTimerAspect {
 	
 	@Pointcut("execution(* com.vetweb.api.resources..*.*(..))")
 	public void everyMethod() {}
-	
+
 	@Around("everyMethod()")
-	public void logAndCount(ProceedingJoinPoint joinPoint) throws Throwable {
+	public Object logAndCount(ProceedingJoinPoint joinPoint) throws Throwable {
 		MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
 		log.info(String.format("Method %s called at %s, return type is %s", 
 				methodSignature.getName(), LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss")), methodSignature.getReturnType()));
 		long start = System.currentTimeMillis();
-		joinPoint.proceed();
+		Object returningObj = joinPoint.proceed();
 		long end = System.currentTimeMillis();
 		long timeToComplete = (start - end)/1000;
-		log.info(String.format("Method took %d to complete execution", timeToComplete));		
+		log.info(String.format("Method took %d to complete execution, returning %s", timeToComplete, returningObj));
+		return returningObj;
 	}
-	
+
 }
